af63ca42ad0066e762a3303c0e2fecf1128a9dd1
Alexandru Carp <105707690+carpuplay@users.noreply.github.com>
2024-02-15 15:14:26 +0100

    revision fichier
    certaines lignes manquantes en v0.1

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,5 +1,36 @@
 package personnages;
 
 public class Druide {
+	private String nom;
+	private int effetPotionMin;
+	private int effetPotionMax;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
+		this.nom = nom;
+		this.effetPotionMin = effetPotionMin;
+		this.effetPotionMax = effetPotionMax;
+		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " + effetPotionMin + " à "
+				+ effetPotionMax + ".");
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+
+	private String prendreParole() {
+		return "Le druide " + nom + " : ";
+	}
+	
+	public static void main(String[] args) {
+		//TODO main pour tester
+	}
 
 }
