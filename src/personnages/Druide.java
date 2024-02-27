db48d740aba3e6772636a51db6c080188e3ed8bb
Alexandru Carp <105707690+carpuplay@users.noreply.github.com>
2024-02-27 19:05:55 +0100

    Revert "Add files via upload"

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,12 +1,17 @@
 package personnages;
 
-import java.util.Random;
-
 public class Druide {
 	private String nom;
 	private int effetPotionMin;
 	private int effetPotionMax;
-	private int forcePotion = 1;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 
 	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
 		this.nom = nom;
@@ -16,33 +21,6 @@ public class Druide {
 				+ effetPotionMax + ".");
 	}
 
-	public int preparerPotion(Random random) {
-		Random random1 = new Random();
-
-		do {
-			forcePotion = random1.nextInt(effetPotionMax);
-
-		} while (!((forcePotion > 4) && (forcePotion < 11)));
-
-		if (forcePotion > 7) {
-			parler("J'ai préparé une super potion de force " + forcePotion);
-//			System.out.println(forcePotion);
-		} else {
-			parler("Je n'ai pas trouvé tous les ingrédients, ma potion est seulement de force " + forcePotion);
-//			System.out.println(forcePotion);
-		}
-		return forcePotion;
-
-	}
-
-	public void booster(Gaulois gaulois) {
-		parler("Non, Obélix !... Tu n’auras pas de potion magique !");
-	}
-
-	public String getNom() {
-		return nom;
-	}
-
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "« " + texte + "»");
 	}
@@ -50,8 +28,6 @@ public class Druide {
 	private String prendreParole() {
 		return "Le druide " + nom + " : ";
 	}
-<<<<<<< HEAD
-=======
 	
 	public static void main(String[] args) {
 	    Druide druide = new Druide("Merlin", 5, 10);
@@ -66,12 +42,5 @@ public class Druide {
 	    
 	    //druide.parler("Je suis un druide très puissant !");
 	}
->>>>>>> 1b1373a67dc9964dddd4e2332ffb058ea8834b17
 
-	public static void main(String[] args) {
-		Druide panoramix = new Druide("Panoramix", 5, 10);
-		Random random1 = new Random();
-		panoramix.preparerPotion(random1);
-
-	}
 }
--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,7 +1,5 @@
 package personnages;
 
-import java.util.Random;
-
 public class Gaulois {
 	private String nom;
 	private int force;
@@ -16,57 +14,29 @@ public class Gaulois {
 		return nom;
 	}
 
-	public int getForce() {
-		return force;
-	}
-
+	
 	public String toString() {
 		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
 	}
 
 	public void parler(String texte) {
-		System.out.println(prendreParole() + "<<" + texte + ">>");
+		System.out.print(prendreParole()+ "<<" + texte + ">>");
 	}
-
+	
 	public String prendreParole() {
-<<<<<<< HEAD
-		return "Le Gaulois" + nom + ":";
-=======
 		return "Le Gaulois " + nom + ":" ;
->>>>>>> 1b1373a67dc9964dddd4e2332ffb058ea8834b17
 	}
-
-	public void boirePotion(int forcePotion) {
-
-		effetPotion = forcePotion;
-		parler("Merci Druide, je sens que ma force est " + forcePotion + " fois décuplée");
-	}
-
+	
 	public void frapper(Romain romain) {
-		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de "
+		+ romain.getNom());
 		romain.recevoirCoup(force / 3);
 	}
-
+	
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		Romain minus = new Romain("Minus", 6);
-		Gaulois asterix = new Gaulois("Asterix", 8);
-		Druide panoramix = new Druide("Panoramix", 5, 10);
-		Random random1 = new Random();
-		int forcePotion = panoramix.preparerPotion(random1);
-
-//		System.out.println(asterix.toString()); 
-
-		System.out.println(asterix.getNom());
-		asterix.parler("Bonjour !");
-		asterix.frapper(minus);
-		asterix.boirePotion(forcePotion);
-=======
 		Gaulois gaulois = new Gaulois("Asterix", 13);
 		
 		gaulois.parler("Ça va les gars?");
 
->>>>>>> 1b1373a67dc9964dddd4e2332ffb058ea8834b17
 	}
-
 }
